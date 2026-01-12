c3fc30243b33086ce984b87081a1f77ed5355e2d
Weeba-it <weebastaff@gmail.com>
2026-01-12 20:44:22 +0000

    Esercizio

--- Esercizio.java
+++ Esercizio.java
@@ -8,32 +8,101 @@ class Esercizio {
     //Input da tastiera
     static Scanner input = new Scanner(System.in);
     
-    //Inserisce il simbolo x oppure o nella grigla di gioco in riga i e colonna j.
-    //Se la mossa non è valida (pedina già presente o le coordinate sono fuori la griglia) allora ritorno falso.
-    static boolean inserisciInGriglia( /*scrivere qui i parametri richiesti dall'esercizio*/ ) {
-        //SCRIVERE QUI IL CODICE RICHIESTO DALL'ESERCIZIO
+    static boolean inserisciInGriglia(String[][] G, int i, int j, String s) {
+        i = i - 1;
+        j = j - 1;
+
+        boolean coordinateCorrette = (i >= 0 && i <= 2 && j >= 0 && j <= 2);
+
+        if (coordinateCorrette && G[i][j].equals("-")) {
+            G[i][j] = s;
+            return true;
+        }
+
+        return false;
     }
 
-    //Azzero la griglia di gioco inserendo la stringa "-" in tutte le celle.
-    static void azzeraGriglia(/*scrivere qui i parametri richiesti dall'esercizio*/ ) {
-        //SCRIVERE QUI IL CODICE RICHIESTO DALL'ESERCIZIO
+    static void azzeraGriglia(String[][] G, int righe, int colonne) {
+        for (int i = 0; i < righe; i++) {
+            for (int j = 0; j < colonne; j++) {
+                G[i][j] = "-";
+            }
+        }
     }
 
-    //Controlla se nella griglia c'è una vincita.
-    // s può valore "O" oppure "X"
-    static boolean controllaVincita(/*scrivere qui i parametri richiesti dall'esercizio*/ ) {
-        //SCRIVERE QUI IL CODICE RICHIESTO DALL'ESERCIZIO
+    static boolean controllaVincita(String[][] G, String s) {
+        for (int i = 0; i < 3; i++) {
+            if ((G[i][0].equals(s) && G[i][1].equals(s) && G[i][2].equals(s)) ||
+                (G[0][i].equals(s) && G[1][i].equals(s) && G[2][i].equals(s))) {
+                return true;
+            }
+        }
+
+        if ((G[0][0].equals(s) && G[1][1].equals(s) && G[2][2].equals(s)) ||
+            (G[0][2].equals(s) && G[1][1].equals(s) && G[2][0].equals(s))) {
+            return true;
+        }
+
+        return false;
     }
 
-    //Conta quante caselle libere ci sono ancora.
-    //Se non ci sono caselle libere e non si è vinto allora è un pareggio
-    static int contaCaselleLibere(/*scrivere qui i parametri richiesti dall'esercizio*/ ) {
-        //SCRIVERE QUI IL CODICE RICHIESTO DALL'ESERCIZIO
-    }
+    static int contaCaselleLibere(String[][] G) {
+        int count = 0;
 
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (G[i][j].equals("-")) {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
 
     public static void main(String args[]) {
-        //SCRIVERE QUI IL CODICE RICHIESTO DALL'ESERCIZIO  
+
+        String[][] griglia = new String[3][3];
+        azzeraGriglia(griglia, 3, 3);
+
+        System.out.println("Griglia di gioco:");
+        UtilsMatrice.visualizza(griglia);
+
+        String s = "X";
+        boolean haiVinto = false;
+        boolean pareggio = false;
+
+        do {
+            boolean mossaCorretta;
+            int mossaI, mossaJ;
+
+            do {
+                System.out.println("Giocatore " + s + ", inserisci la tua mossa (riga e colonna [1-3]):");
+                mossaI = Integer.parseInt(input.nextLine());
+                mossaJ = Integer.parseInt(input.nextLine());
+                mossaCorretta = inserisciInGriglia(griglia, mossaI, mossaJ, s);
+            } while (!mossaCorretta);
+
+            System.out.println("Griglia di gioco:");
+            UtilsMatrice.visualizza(griglia);
+
+            haiVinto = controllaVincita(griglia, s);
+            if (haiVinto) {
+                System.out.println(s + " VINCE!");
+            }
+
+            pareggio = !haiVinto && contaCaselleLibere(griglia) == 0;
+            if (pareggio) {
+                System.out.println("PAREGGIO!");
+            }
+
+            if (s.equals("X")) {
+                s = "O";
+            } else {
+                s = "X";
+            }
+
+        } while (!haiVinto && !pareggio);
     }
 }
 
